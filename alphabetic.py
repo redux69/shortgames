b4f179eb5dffc7931a35c8fcf993813e14c4053d
redux69 <60585477+redux69@users.noreply.github.com>
2020-05-02 17:14:57 +0300

    Create alphabetic.py

---
+++ alphabetic.py
@@ -0,0 +1,7 @@
+# here you can put any word you want, this will put your words in an alphabetic row
+my_str = "zat am bani duba ooo ea face hulala cu iala yala jean gaura kapat windows xurici lautar naprasnic matura qacat rau satul tau uganda paula vineri"
+words = my_str.split()
+words.sort()
+print("Alfabetic:")
+for word in words:
+   print(word)
